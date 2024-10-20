78d937a9b8c169b82594559e9fe052b4f4e0326e
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2024-10-20 01:21:41 -0400

    Refactor index.html and Stopwatch.tsx
    - Remove unnecessary whitespace in index.html
    - Add stopwatch functionality to Stopwatch.tsx
    - Import and use Helpers class in Stopwatch.tsx
    - Implement time conversion function in Helpers class

--- src/components/timers/Stopwatch.tsx
+++ src/components/timers/Stopwatch.tsx
@@ -1,3 +1,49 @@
-const Stopwatch = () => null;
+import { useState, useRef } from "react";
+import Helpers from "../../utils/helpers";
+
+
+
+const Stopwatch = () => {
+
+    const [time, setTime] = useState<number>(0);
+    const status = useRef('stop');
+    const prom = new Promise(() => {
+        console.log(status.current);
+        setTimeout(() => {
+            if(status.current === 'run') {
+                setTime(time + 1000);
+            }
+        }, 1000);   
+    });
+    
+    
+    
+      
+
+    return (
+        <div>
+            <h1>{Helpers.milisecondsToTime(time)}</h1>
+            <button onClick={() => {
+                status.current = 'run';
+                console.log(status.current);
+            }}>▶️</button>
+
+            <button onClick={() => {
+                console.log(status.current);
+                if (status.current !== 'pause') {
+                    status.current = 'pause';
+                } else {
+                    status.current = 'run';
+                };
+                console.log(status.current);
+            }}>⏸️</button>
+
+            <button onClick={() => {
+                status.current = 'stop';
+                setTime(0);
+            }}>⏹️</button>                    
+        </div>
+    );
+};
 
 export default Stopwatch;
