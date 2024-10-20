319cdbc4d833e1a64b5090fcf58b7ea7e301a62b
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2024-10-19 23:16:23 -0400

    Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -11,20 +11,21 @@ import "./index.css";
 import TimersView from "./views/TimersView";
 import DocumentationView from "./views/DocumentationView";
 
+
 const PageIndex = () => {
   return (
-    <div>
-      <h1>Assignment</h1>
-      <ul>
-        <li>
-          <Link to="/">Timers</Link>
-        </li>
-        <li>
-          <Link to="/docs">Documentation</Link>
-        </li>
-      </ul>
-      <Outlet />
-    </div>
+    <>
+      <div className = "bg-gray-800 mx-auto px-6 py-3 flex items-center">
+        <div className="flex space-x-4">
+          <h1 className = "text-3xl font-bold text-white">Assignment 1</h1>     
+          <Link className = "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" to="/">Timers</Link>
+          <Link className = "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" to="/docs">Documentation</Link>
+         </div>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
   );
 };
 
