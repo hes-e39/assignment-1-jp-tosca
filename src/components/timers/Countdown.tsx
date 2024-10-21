ab677c9dff74f2c117e5f0e1f1f36e9e37d00d72
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2024-10-21 00:43:10 -0400

    Countdown working

--- src/components/timers/Countdown.tsx
+++ src/components/timers/Countdown.tsx
@@ -1,3 +1,43 @@
-const Countdown = () => null;
+import { useState, useRef } from "react";
+import Helpers from "../../utils/helpers";
+
+type CountdownProps = {
+    initTime: number;
+};
+
+const Countdown = ({initTime}: CountdownProps) => {
+
+    
+
+    const [time, setTime] = useState<number>(initTime);
+    const status = useRef('stop');
+    const intervalRef = useRef<number | undefined>(undefined);
+
+    const onStart = () => {
+        if (status.current === 'start') return
+        status.current = 'start';
+        intervalRef.current = setInterval(() => {
+            setTime((currentTime) => currentTime - 50)
+        }, 50);
+    }
+
+    return (
+        <div>
+            <h1>{Helpers.milisecondsToTime(time)}</h1>
+            <button onClick={onStart}>▶️</button>
+
+            <button onClick={() => {
+               status.current = 'pause';
+               clearInterval(intervalRef.current);
+            }}>⏸️</button>
+
+            <button onClick={() => {
+                status.current = 'stop';
+                setTime(initTime);
+                clearInterval(intervalRef.current);
+            }}>⏹️</button>                    
+        </div>
+    );
+};
 
 export default Countdown;
